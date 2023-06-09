a9b4b4bb6fd35fbf76e3d7176d0b16fc3337ef73
scshiv29-dev <shivchaudhary790@hotmail.com>
2023-06-09 22:13:32 +0530

    added seed.js

--- packages/appwrite/seed.js
+++ packages/appwrite/seed.js
@@ -10,17 +10,18 @@ const client = new Client()
 
 const db= new Databases(client);    
 const users=new Users(client);
+
+const createUser=async()=>{
+    const user=await users.create(process.env.USERID,process.env.USER_EMAIL,process.env.USER_PHONE,process.env.USER_PASSWORD,process.env.USER_NAME);
+}
 const createDB=async()=>{
-    const dbList=await db.list();
-    if(dbList.databases.length===0){
         await db.create(proccess.env.APPWRITE_DB_ID,"mainDB").then(console.log).catch(console.error);
-    }
+    
 }
 
-const createCollections=async()=>{
-    const collectionList=await db.listCollections(process.env.APPWRITE_COLLECTION_ID||"");
-    if(collectionList.collections.length===0){
-        await db.createCollection(proccess.env.APPWRITE_DB_ID,"flexiDB-databses","Databases",[
+const createCollection=async()=>{
+
+        await db.createCollection(proccess.env.APPWRITE_DB_ID,process.env.APPWRITE_DB_COLLECTION_DB_ID,"Databases",[
             Permission.read(Role.user(process.env.USERID)),
             Permission.create(Role.user(process.env.USERID)),
             Permission.update(Role.user(process.env.USERID)),
@@ -28,33 +29,21 @@ const createCollections=async()=>{
         ]) 
         .then(async (res)=>{
             console.log(res);       
-            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-databses","name",100,true)
-            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-databses","type",100,true)
-            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-databses","tag",15,true)
-            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-databses","containerId",15,true)
-
-
+            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,process.env.APPWRITE_DB_COLLECTION_DB_ID,"name",100,true)
+            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,process.env.APPWRITE_DB_COLLECTION_DB_ID,"type",100,true)
+            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,process.env.APPWRITE_DB_COLLECTION_DB_ID,"tag",15,true)
+            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,process.env.APPWRITE_DB_COLLECTION_DB_ID,"containerId",15,true)
         }).catch((err)=>{
             console.log(err);
         });
 
-        await db.createCollection(proccess.env.APPWRITE_DB_ID,"flexiDB-settings","Settings",["*"])
-        .then(async (res)=>{
-            console.log(res);       
-            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-settings","name",100,true,"")
-            await db.createStringAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-settings","url",150,true,"")
-            await db.createBooleanAttribute(proccess.env.APPWRITE_DB_ID,"flexiDB-settings","isRegable",false,true)
-            await db.createDocument(proccess.env.APPWRITE_DB_ID,"flexiDB-settings","main",{
-                "name":"FlexiDB",
-                "url":"",
-                "isRegable":false
-            }).then(console.log).catch(console.error);
-        }).catch((err)=>{
-            console.log(err);
-        });
-}
 }
 
-const createUser=async()=>{
-    const user=await users.create(process.env.USERID,process.env.USER_EMAIL,process.env.USER_PHONE,process.env.USER_PASSWORD,process.env.USER_NAME);
+
+
+const seed=async()=>{
+    await createUser();
+    await createDB();
+    await createCollection();
+
 }
